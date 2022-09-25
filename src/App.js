5eab1923d7dcdf115f77a56e9ebb18d4d797ce37
alecup <skrirepi@gmai.com>
2022-09-25 04:51:56 -0300

    probando. Cupare Ramirueba002

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1> hola mundo</h1>
+      <h1> hola mundo prueba 001</h1>
       </header>
     </div>
   );
